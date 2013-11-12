5b905526660a826a8aa94536dd9f3dda94e071f9
Dan Williams Robledo Cruz <dan.rocross@gmail.com>
2013-11-12 12:53:33 -0600

    Tare 8 v1.3

--- Tarea_7/imnoises.m
+++ Tarea_7/imnoises.m
@@ -2,48 +2,48 @@ function Salida=imnoises(I,opcion,a,b)% la función se llama imhistogram
 
 switch lower(opcion)
     case'uniform'
-        U = rand(size(I));
-        Salida=a+(b-a)*U;
-        Salida=I+Salida;
+        U = rand(size(I));      %se crean numeros aleatorios uniforme
+        Salida=a+(b-a)*U;       %se aplica la formula para el ruido uniform
+        Salida=I+Salida;        %se suma el ruido a la imagen original
     case'gaussian'
-        N = randn(size(I));
-        Salida=a+(b-a)*N;
-        Salida=I+Salida;
+        N = randn(size(I));     %se crean numeros aleatorios normales media 0 varianza 1
+        Salida=a+(b-a)*N;       %se aplica la formula para el ruido gaussiano
+        Salida=I+Salida;        %se suma el ruido a la imagen original
     case'rayleigh'
-        U = rand(size(I));
-        Salida = a+sqrt(-b*log(1-U));
-        Salida=I+Salida;
+        U = rand(size(I));              %se crean numeros aleatorios uniforme
+        Salida = a+sqrt(-b*log(1-U));   %se aplica la formula para el ruido rayleigh
+        Salida=I+Salida;                %se suma el ruido a la imagen original
     case'lognormal'
-        N = randn(size(I));
-        Salida = exp(b*N + a);
-        Salida=mat2gray(I+Salida);        
+        N = randn(size(I));             %se crean numeros aleatorios normales media 0 varianza 1
+        Salida = exp(b*N + a);          %se aplica la formula para el ruido lognormal
+        Salida=I+mat2gray(Salida);      %se suma el ruido a la imagen original 
     case'exponential'
-        U = rand(size(I));
+        U = rand(size(I));              %se crean numeros aleatorios uniforme
         Salida = (-1/a)*(log(1-U));
-        Salida=I+Salida;
+        Salida=I+Salida;                %se suma el ruido a la imagen original
     case'erlang'
-        if(b~=round(b)||b<=0)
+        if(b~=round(b)||b<=0)           %B tiene que ser un numero entero positivo
             disp('B tiene que ser un entero positivo, para el erlang');
             Salida=-1;
         else
             Salida=zeros(size(I));
             for j=1:b
-                Salida = Salida+(-1/a)*log(1-rand(size(I)));
+                Salida = Salida+(-1/a)*log(1-rand(size(I)));     %Sumatoria de numeros aleatorios exponeciales de 1 hasta B
             end
-            Salida=mat2gray(I+Salida);
+            Salida=I+mat2gray(Salida);                          %se suma el ruido a la imagen original
         end
     case'salt&pepper'
-        if(a+b)>1
+        if(a+b)>1                   %la suma de a+b no debe ser mayor a 1
             disp('La suma de a+b no debe ser mayor a 1, para el salt&pepper');
             Salida=-1;
         
         else
-            U = rand(size(I));
-            c=find(U<=a);
+            U = rand(size(I));      %se crean numeros aleatorios uniforme
+            c=find(U<=a);           %se obtienen los numeros menores de <a> para volverlos pepper 
             I(c)=0;
-            c=find(U>a&U<=a+b);
+            c=find(U>a&U<=a+b);      %se obtienen los numeros mayores de <a> pero menores que <a+b> para volverlos salt
             I(c)=1;
-            Salida=I;
+            Salida=I;               %se retorna la imagen con ruido
         end
     otherwise
         disp('Opcion incorrecta')
--- Tarea_7/main.m
+++ Tarea_7/main.m
@@ -15,40 +15,40 @@
 clear all; clc; close all; %Limpia todo antes de empezar
 
 
-img = imread('cameraman.tif');
+img = imread('cameraman.tif');       %Lee imagen
 I=mat2gray(img);   
 
-Resultado=imnoises(I,'uniform',0,0.5);
+Resultado=imnoises(I,'uniform',0,0.5);  %Manda a llamar ruido uniform
 figure
 imshow(Resultado)
 title('uniforme')
 
-Resultado=imnoises(I,'gaussian',0,0.3);
+Resultado=imnoises(I,'gaussian',0,0.3); %Manda a llamar Ruido gaussian
 figure
 imshow(Resultado)
 title('gausiano')
 
-Resultado=imnoises(I,'lognormal',0,0.1);
+Resultado=imnoises(I,'lognormal',0,0.1);    %Manda a llamar lognormal
 figure
 imshow(Resultado)
 title('lognormal')
 
-Resultado=imnoises(I,'rayleigh',0,0.1);
+Resultado=imnoises(I,'rayleigh',0,0.1);     %Manda a llamar rayleigh
 figure
 imshow(Resultado)
 title('raylengt')
 
-Resultado=imnoises(I,'exponential',5,[]);
+Resultado=imnoises(I,'exponential',5,[]);   %Manda a llamar exponential
 figure
 imshow(Resultado)
 title('exponencial')
 
-Resultado=imnoises(I,'erlang',5,10);
+Resultado=imnoises(I,'erlang',5,10);        %Manda a llamar erlang
 figure
 imshow(Resultado)
 title('erlang')
 
-Resultado=imnoises(I,'salt&pepper',0.05,0.05);
+Resultado=imnoises(I,'salt&pepper',0.05,0.05);  %Manda a llamar Salt&Pepper
 if Resultado~=-1
 figure
 imshow(Resultado)
