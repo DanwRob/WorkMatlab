320cd7dc853a975ec72374873c13dea11008b8e3
Dan Williams <cti201108@DanWro.local>
2014-01-20 18:38:25 -0600

    Proyecto_codificacion

---
+++ test.m
@@ -0,0 +1,39 @@
+clear all, close all, clc
+
+intervalo=0.01;
+[x,y]=meshgrid(0:intervalo:1);
+z=ones(size(x));
+z=1-(x+y);
+
+f=find(z<0);
+z(f)=NaN;
+
+alfa=(-x.*log2(x));
+f1=find(isnan(alfa));
+alfa(f1)=0;
+
+beta=(-y.*log2(y));
+f1=find(isnan(beta));
+beta(f1)=0;
+
+fab=(-z.*log2(z));
+f1=find(isnan(fab));
+fab(f1)=0;
+
+Hs3=alfa+beta+fab;
+Hs3(f)=NaN;
+lz=(0:intervalo:max(Hs3(:))+.1);
+lx=ones(size(lz));
+lx(:)=0.33;
+ly=(0:intervalo:1/3);
+
+figure
+
+surf(x,y,Hs3,'FaceColor','red', 'EdgeColor','none')
+camlight left; lighting phong
+hold on
+line(lx,lx,lz,'linestyle', '--')
+lz(:)=max(Hs3(:));
+line(ly,lx(1:length(ly)),lz(1:length(ly)),'linestyle', '--')
+
+
